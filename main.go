375464b265b1d71e46221991793f93196b26e213
kbaik <>
2025-10-24 12:57:41 -0700

    Initialize simple fileserver at localhost

--- main.go
+++ main.go
@@ -1,22 +1,62 @@
 package main
 
 import (
-    "net/http"
+//    "sync/atomic"
+//    "io"
     "log"
+    "net/http"
+//    "fmt"
 
 )
 
 func main() {
+    const filepathRoot = "."
+    const port = "8080"
+    
+    /*
+    apiCfg := apiConfig{
+	fileserverHits: atomic.Int32{},
+    }
+    */
     mux := http.NewServeMux()
-    mux.Handle("/", http.FileServer(http.Dir(".")))
+    mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+    /*
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
+    })
+    mux.Handle("/metrics", apiCfg.handlerMetrics(func(w http.ResponseWriter, req *http.Request)))
+    */
+
+
     server := http.Server{
-	Addr: ":8080",
+	Addr: ":" + port,
 	Handler: mux,
     }
-    
-    err := server.ListenAndServe()
-    if err != nil {
-	log.Fatalf("ListenAndServe Error: %w", err)
-    }
+
+    log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+    log.Fatal(server.ListenAndServe())
 
 }
+
+/*
+type apiConfig struct {
+    fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    cfg.fileserverHits.Add(1)
+    return next
+}
+
+func (cfg *apiConfig) handlerMetrics(func(w http.ResponseWriter, req *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request){
+	    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	    w.WriteHeader(http.StatusOK)
+	    io.WriteString(w, fmt.Sprintf("Hits: %v", cfg.fileserverHits))
+	}
+}
+*/
+
